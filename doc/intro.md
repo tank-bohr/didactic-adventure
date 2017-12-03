d2aff0a8e9b544933657988c85067f44e5114c22
Alexey Nikitin <nikitin@bookmate.com>
2017-12-03 14:21:33 +0300

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to didactic-adventure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/didactic_adventure/core_test.clj
@@ -0,0 +1,7 @@
+(ns didactic-adventure.core-test
+  (:require [clojure.test :refer :all]
+            [didactic-adventure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
