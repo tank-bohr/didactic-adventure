aaeb9cc3b07d44ecc2f026b58b25a778c088eff2
Alexey Nikitin <nikitin@bookmate.com>
2018-01-17 18:33:47 +0300

    dummy echo bot

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.8.0"]
-                 [org.immutant/web "2.1.9"]
+                 [org.clojure/data.json "0.2.6"]
+                 [http-kit "2.2.0"]
                  ]
   :main ^:skip-aot didactic-adventure.core
   :target-path "target/%s"
--- src/didactic_adventure/core.clj
+++ src/didactic_adventure/core.clj
@@ -1,11 +1,53 @@
 (ns didactic-adventure.core
   (:require
-    [immutant.web :as web])
+    [clojure.data.json :as json]
+    [org.httpkit.server :as web]
+    [org.httpkit.client :as http])
   (:gen-class))
 
-(defn app [request]
+(def token
+  (System/getenv "TOKEN"))
+
+(def port
+  (System/getenv "PORT" ))
+
+(defn get-updates-url []
+  (str "https://api.telegram.org/" token "/getUpdates"))
+
+(defn send-message-url []
+  (str "https://api.telegram.org/" token "/sendMessage"))
+
+(defn get-updates []
+  (->
+    (get-updates-url)
+    http/post
+    deref
+    :body
+    (json/read-str :key-fn keyword)
+    :result))
+
+(defn send-message [chat-id text]
+  (->
+    (send-message-url)
+    (http/post {:form-params
+                {:chat_id chat-id
+                 :text text}})
+    deref
+    :body
+    (json/read-str :key-fn keyword)
+    :result))
+
+(defn handle-update [update]
+  (let [chat-id (-> update :message :chat :id)
+        text    (-> update :message :text)]
+    (send-message chat-id text)))
+
+(defn run []
+  (map handle-update (get-updates)))
+
+(defn app [_request]
   {:status 200
    :body "Welcome"})
 
 (defn -main []
-  (web/run app))
+  (web/run-server app {:port port}))
