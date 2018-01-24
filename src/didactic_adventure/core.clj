f8b9bf0208cef6f0c78ab49ca4fc8a853a55e7cb
Alexey Nikitin <nikitin@bookmate.com>
2018-01-25 00:57:37 +0300

    Move telegram to separate namespace. Fixes #2

--- src/didactic_adventure/core.clj
+++ src/didactic_adventure/core.clj
@@ -1,68 +1,20 @@
 (ns didactic-adventure.core
   (:require
-    [clojure.data.json :as json]
     [clojure.tools.logging :as log]
-    [org.httpkit.server :as web]
     [didactic-adventure.slack :as slack]
-    [didactic-adventure.http :as http]
-    [didactic-adventure.brain :as brain])
+    [didactic-adventure.telegram :as telegram])
   (:gen-class))
 
 (defn port []
   (-> "PORT" System/getenv read-string))
 
-(defn token []
-  (->> "TOKEN" System/getenv (str "bot")))
-
-(defn webhook-token []
-  (System/getenv "WEBHOOK_TOKEN"))
-
 (defn slack-token []
   (System/getenv "SLACK_TOKEN"))
-
-(defn send-message-url []
-  (str "https://api.telegram.org/" (token) "/sendMessage"))
-
-(defn send-message [chat-id text]
-  (->
-    (send-message-url)
-    (http/post {:chat_id chat-id
-                :text text})
-    :result))
-
-(defn reply-to [message]
-  (let [chat-id  (-> message :chat :id)
-        text     (-> message :text)
-        reaction (brain/react text)]
-    (if-not
-      (nil? reaction) (do
-                        (log/info "reply-to: " message)
-                        (send-message chat-id reaction)))))
-
-(defn webhook-arrived [request]
-  (let [raw-body (slurp (:body request))
-        webhook (json/read-str raw-body :key-fn keyword)
-        message (:message webhook)]
-    (do
-      (log/info "webhook-arrived: " webhook)
-      (reply-to message)
-      {:status 204})))
-
-(defn forbidden []
-  {:status 403})
-
-(defn app [request]
-  (let [uri (:uri request)]
-    (do
-      (log/info uri " requested")
-      (if (= uri (str "/" (webhook-token)))
-        (webhook-arrived request)
-        (forbidden)))))
 
 (defn -main []
   (let [port (port)
         slack-token (slack-token)]
-    (log/info "Starting server. Listen port " port "...")
+    (log/info "Starting...")
     (if-not (nil? slack-token)
       (slack/connect slack-token))
-    (web/run-server app {:port port})))
+    (telegram/start port)))
---
+++ src/didactic_adventure/telegram.clj
@@ -0,0 +1,54 @@
+(ns didactic-adventure.telegram
+  (:require
+    [clojure.data.json :as json]
+    [clojure.tools.logging :as log]
+    [org.httpkit.server :as server]
+    [didactic-adventure.http :as http]
+    [didactic-adventure.brain :as brain]))
+
+(def token (memoize (fn []
+  (->> "TOKEN" System/getenv (str "bot")))))
+
+(def webhook-url (memoize (fn []
+  (->> "WEBHOOK_TOKEN" System/getenv (str "/")))))
+
+(def send-message-url (memoize (fn []
+  (str "https://api.telegram.org/" (token) "/sendMessage"))))
+
+(defn send-message [chat-id text]
+  (->
+    (send-message-url)
+    (http/post {:chat_id chat-id
+                :text text})
+    :result))
+
+(defn reply-to [message]
+  (let [chat-id  (-> message :chat :id)
+        text     (-> message :text)
+        reaction (brain/react text)]
+    (if-not
+      (nil? reaction) (do
+                        (log/info "reply-to: " message)
+                        (send-message chat-id reaction)))))
+
+(defn webhook-arrived [request]
+  (let [raw-body (slurp (:body request))
+        webhook (json/read-str raw-body :key-fn keyword)
+        message (:message webhook)]
+    (log/info "webhook-arrived: " webhook)
+    (reply-to message)
+    {:status 204}))
+
+(defn forbidden []
+  {:status 403})
+
+(defn app [request]
+  (let [uri (:uri request)]
+    (log/info uri " requested")
+    (if (= uri (webhook-url))
+      (webhook-arrived request)
+      (forbidden))))
+
+(defn start [port]
+  (log/info "Starting server. Listen port " port "...")
+  (server/run-server app {:port port}))
